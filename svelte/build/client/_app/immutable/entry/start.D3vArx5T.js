21660d6fb3454dc93879ba9ee921c79f92bbd6b5
Juan Francisco Villasmil Tovar <juanvillans16@gmail.com>
2024-03-23 17:12:58 -0400

    vercel.json

---
+++ svelte/build/client/_app/immutable/entry/start.D3vArx5T.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CGkob4Cg.js";export{t as start};
---
+++ svelte/build/server/chunks/1-Lr44WwtC.js
@@ -0,0 +1,9 @@
+const index = 1;
+let component_cache;
+const component = async () => component_cache ??= (await import('./error.svelte-DErZZhHT.js')).default;
+const imports = ["_app/immutable/nodes/1.g72y-GaE.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.fi1e1Nz5.js","_app/immutable/chunks/entry.CGkob4Cg.js"];
+const stylesheets = [];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=1-Lr44WwtC.js.map
