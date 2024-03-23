078baf68a7a43e82e10bec1e3c07aceacbcf000b
Juan Francisco Villasmil Tovar <juanvillans16@gmail.com>
2024-03-23 16:19:49 -0400

    svelte + express

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import {handler} from "./svelte/build/handler.js"
+
+const app = express();
+const port = 3000;
+
+// demo route
+app.get('/expressroute', (req, res) => {
+  res.send("This is an express route");  
+});
+
+// This will connect your svelte kit app to your express server
+app.use(handler)
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+}); //
